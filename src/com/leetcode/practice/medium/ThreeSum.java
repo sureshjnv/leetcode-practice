9dafe4c16197745bae7aa2f93e4a9597a920eaf0
sureshmoida <sureshjnvvzm1@gmail.com>
2025-08-11 22:17:09 +0530

    BugFix ThreeSum

--- src/com/leetcode/practice/medium/ThreeSum.java
+++ src/com/leetcode/practice/medium/ThreeSum.java
@@ -16,11 +16,12 @@ public class ThreeSum {
 		Arrays.sort(nums);
 		
 		for(int i = 0 ; i < nums.length -2; i++) {
+			
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
 			int left = i+1;
 			int right = nums.length -1;
 			int target = -nums[i];
-			
-			if(left > 0 && nums[left] == nums[left-1]) continue;
 			
 			while(left < right) {
 				int sum = nums[left] + nums[right];
