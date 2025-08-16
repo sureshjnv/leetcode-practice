0ec7062b5b85558fefc817432592eef735bd911e
sureshmoida <sureshjnvvzm1@gmail.com>
2025-08-16 20:07:04 +0530

    Solved Swap Nodes In Pair

---
+++ src/com/leetcode/practice/medium/SwapNodesInPair.java
@@ -0,0 +1,27 @@
+package com.leetcode.practice.medium;
+
+import com.leetcode.utility.ListNode;
+
+public class SwapNodesInPair {
+
+	public ListNode swapPairs(ListNode head) {
+		
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		
+		ListNode point = dummy;
+		
+		while(point.next != null && point.next.next != null) {
+			
+			ListNode swap1 = point.next;
+			ListNode swap2 = point.next.next;
+			
+			swap1.next = swap2.next;
+			swap2.next = swap1;
+			
+			point.next = swap2;
+			point = swap1;
+		}
+		return dummy.next;
+	}
+}
