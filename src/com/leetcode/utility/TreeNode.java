a2cdcc3bd0bb2a9490c70fd04b5380be81fc298f
sureshmoida <sureshjnvvzm1@gmail.com>
2025-08-27 19:42:47 +0530

    Implemented Binary tree traversals Iterative way

--- src/com/leetcode/utility/TreeNode.java
+++ src/com/leetcode/utility/TreeNode.java
@@ -1,5 +1,7 @@
 package com.leetcode.utility;
 
+import java.util.Stack;
+
 public class TreeNode {
 
 	public int val;
@@ -41,6 +43,94 @@ public class TreeNode {
 		inOrderTraversal(root.right);
 	}
 
+	public static void inOrderIterativeTraversal(TreeNode root) {
+		Stack<TreeNode> stack = new Stack<>();
+		TreeNode curr = root;
+
+		while (true) {
+			if (curr != null) {
+				stack.push(curr);
+				curr = curr.left;
+			} else {
+				if (stack.isEmpty()) {
+					break;
+				}
+				curr = stack.pop();
+				System.out.print(curr.val + " -> ");
+				curr = curr.right;
+			}
+		}
+	}
+
+	public static void preOrderIterativeTraversal(TreeNode root) {
+		Stack<TreeNode> stack = new Stack<>();
+		if (root == null)
+			return;
+		stack.push(root);
+
+		while (!stack.isEmpty()) {
+
+			TreeNode curr = stack.pop();
+			System.out.print(curr.val + " -> ");
+			if (curr.right != null) {
+				stack.push(curr.right);
+			}
+			if (curr.left != null) {
+				stack.push(curr.left);
+			}
+		}
+	}
+
+	public static void postOrderIterativeTraversalTwoStack(TreeNode root) {
+
+		Stack<TreeNode> stack1 = new Stack<>();
+		Stack<TreeNode> stack2 = new Stack<>();
+		if (root == null)
+			return;
+		stack1.push(root);
+
+		while (!stack1.isEmpty()) {
+
+			TreeNode curr = stack1.pop();
+			stack2.push(curr);
+			if (curr.left != null) {
+				stack1.push(curr.left);
+			}
+			if (curr.right != null) {
+				stack1.push(curr.right);
+			}
+
+		}
+
+		while (!stack2.isEmpty()) {
+			System.out.println(stack2.pop().val + " -> ");
+		}
+	}
+
+	public static void postOrderIterativeTraversalOneStack(TreeNode root) {
+
+		Stack<TreeNode> stack = new Stack<>();
+		TreeNode curr = root;
+		TreeNode lastVisited = null;
+		
+		while(curr != null || !stack.isEmpty()) {
+			if(curr != null) {
+				stack.add(curr);
+				curr = curr.left;
+			}
+			else {
+				TreeNode peekNode = stack.peek();
+				if(peekNode.right != null && lastVisited != peekNode.right) {
+					curr = peekNode.right;
+				}
+				else {
+					System.out.print(peekNode.val+"->");
+					lastVisited = stack.pop();
+				}
+			}
+		}
+	}
+
 	public static boolean search(TreeNode root, int key) {
 		if (root == null)
 			return false;
@@ -59,7 +149,10 @@ public class TreeNode {
 
 		int[] nodes = new int[] { 3, 2, 1, 5, 6, 4 };
 		TreeNode root = buildBST(nodes);
-		inOrderTraversal(root);
+		inOrderIterativeTraversal(root);
+		preOrderIterativeTraversal(root);
+		postOrderIterativeTraversalTwoStack(root);
+		postOrderIterativeTraversalOneStack(root);
 		System.out.println(search(root, 15));
 
 	}
